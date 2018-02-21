dc6db18df43244c7f32e280b2e8774a4b81ce53b
Paul <Yipu@appletekiMacBook-Pro.local>
2018-02-21 15:59:25 -0700

    userTest test.

---
+++ app/src/test/java/com/example/taskmagic/testUsers.java
@@ -0,0 +1,13 @@
+package com.example.taskmagic;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * Created by Yipu on 21/02/2018.
+ */
+
+public class testUsers extends ActivityInstrumentationTestCase2 {
+    public testUsers() {
+        super(MainActivity.class);
+    }
+}
