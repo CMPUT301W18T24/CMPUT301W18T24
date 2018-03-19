0cf752da4fff4e69cb0f4f3d10d3b806426a021e
Steven Yan <ssyan@ualberta.ca>
2018-03-18 19:23:49 -0600

    profile changes

--- app/src/main/java/com/example/taskmagic/ViewProfileActivity.java
+++ app/src/main/java/com/example/taskmagic/ViewProfileActivity.java
@@ -4,12 +4,14 @@ import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -27,56 +29,55 @@ public class ViewProfileActivity extends AppCompatActivity {
     private DatabaseReference db;
     private FirebaseAuth auth;
 
+    private BottomNavigationView mProfileNav;
+    private FrameLayout mProfileFrame;
+    private bids_frag bidFragment;
+    private assigned_frag assignFragment;
+    private user_profile_frag userFragment;
+    private requested_frag requestFragment;
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_homefeed);
-        UserSingleton singleton = UserSingleton.getInstance();
-        auth = singleton.getmAuth();
-        fmanager = new FireBaseManager(singleton.getmAuth(), getApplicationContext());
+        setContentView(R.layout.view_profile);
+        mProfileNav = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        mProfileFrame = (FrameLayout) findViewById(R.id.user_fragment_container);
+        bidFragment = new bids_frag();
+        assignFragment = new assigned_frag();
+        userFragment = new user_profile_frag();
+        requestFragment = new requested_frag();
 
-        user_profile_frag user_frag = new user_profile_frag();
-        FragmentManager fragmentManager = getFragmentManager();
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.add(R.id.user_fragment_container, user_frag);
-        transaction.commit();
+        setFragment(userFragment);
 
-        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
-
-        bottomNavigationView.setOnNavigationItemSelectedListener(
+        mProfileNav.setOnNavigationItemSelectedListener(
                 new BottomNavigationView.OnNavigationItemSelectedListener() {
                     @Override
                     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                         switch (item.getItemId()) {
                             case R.id.my_info:
-                                user_profile_frag user_frag = new user_profile_frag();
-                                FragmentManager fragmentManager = getFragmentManager();
-                                FragmentTransaction transaction = fragmentManager.beginTransaction();
-                                transaction.replace(R.id.user_fragment_container, user_frag);
-                                transaction.commit();
-                                break;
+                                setFragment(userFragment);
+                                return true;
                             case R.id.requested_task:
-                                requested_frag req_frag = new requested_frag();
-                                FragmentManager fragmentManager1 = getFragmentManager();
-                                FragmentTransaction transaction1 = fragmentManager1.beginTransaction();
-                                transaction1.replace(R.id.user_fragment_container, req_frag);
-                                transaction1.commit();
-                                break;
+                                setFragment(requestFragment);
+                                return true;
                             case R.id.assigned_task:
-                                assigned_frag ass_frag = new assigned_frag();
-                                FragmentManager fragmentManager2 = getFragmentManager();
-                                FragmentTransaction transaction2 = fragmentManager2.beginTransaction();
-                                transaction2.replace(R.id.user_fragment_container, ass_frag);
-                                break;
+                                setFragment(assignFragment);
+                                return true;
                             case R.id.bidded_task:
-                                bids_frag bid_frag = new bids_frag();
-                                FragmentManager fragmentManager3 = getFragmentManager();
-                                FragmentTransaction transaction3 = fragmentManager3.beginTransaction();
-                                transaction3.replace(R.id.user_fragment_container, bid_frag);
-                                break;
+                                setFragment(bidFragment);
+                                return true;
+
+                            default: return false;
                         }
-                        return true;
                     }
+
                 });
     }
 
+    private void setFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.user_fragment_container, fragment);
+        transaction.commit();
+    }
 }
